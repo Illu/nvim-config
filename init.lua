3d2a596a148834256fed0ec11e645fe8fb0e558b
Maxime Nory <mail@maximenory.com>
2024-11-18 21:32:23 -0500

    close terminal pane

--- init.lua
+++ init.lua
@@ -10,6 +10,7 @@ vim.keymap.set('t', '<C-w>h', [[<C-\><C-n><C-w>h]])
 vim.keymap.set('t', '<C-w>j', [[<C-\><C-n><C-w>j]])
 vim.keymap.set('t', '<C-w>k', [[<C-\><C-n><C-w>k]])
 vim.keymap.set('t', '<C-w>l', [[<C-\><C-n><C-w>l]])
+vim.keymap.set('t', '<C-w>q', [[<C-\><C-n><C-w>q]])
 
 vim.api.nvim_create_autocmd({"TermOpen", "BufEnter"}, {
     pattern = "term://*",
