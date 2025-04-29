afe48015282e6559ea857f310331d20e7f6615d3
Maxime Nory <mail@maximenory.com>
2025-04-28 21:56:13 -0400

    telescope remaps

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,10 +2,10 @@ return {
     'nvim-telescope/telescope.nvim',
 	dependencies = { 'nvim-lua/plenary.nvim' },
 	keys = {
-		{"<leader>o", "<cmd>Telescope find_files<cr>", {}},
-		{"<leader>s", "<cmd>Telescope live_grep<cr>", {}},
-		{"<C-o>", "<cmd>Telescope git_files<cr>", {}},
-        -- does not work
-        -- {"<leader>s", "y<ESC>:Telescope live_grep default_text=<c-r>0<CR>", {mode = "v"}}
+		{"<leader>ff", "<cmd>Telescope find_files<cr>", {}},
+		{"<leader>fg", "<cmd>Telescope live_grep<cr>", {}},
+		{"<leader>fs", "<cmd>Telescope grep_string<cr>", {}},
+		{"<leader>fb", "<cmd>Telescope buffers<cr>", {}},
+		{"<C-o>", "<cmd>Telescope git_files<cr>", {}}
 	}
 }
